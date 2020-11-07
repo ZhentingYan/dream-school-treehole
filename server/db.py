b1570763d46452e158fb83fec7682adacdbd6a35
Sun Haoran <ludanxer@gmail.com>
2020-11-07 17:58:19 +0100

    feat: enable auth

--- server/db.py
+++ server/db.py
@@ -1,14 +1,18 @@
 from pymongo import MongoClient
 from flask import current_app, g
 
+
 def get_db():
     if 'db' not in g:
-        g.db = MongoClient('mongodb://localhost:27017/')
+        g.db = MongoClient(host='localhost', port=27017,
+                           username='myUserAdmin', password='tjusse2020')
     return g.db['treehole']
+
 
 def get_collection(collection_name):
     db = get_db()
     return db[collection_name]
+
 
 def close_db(e=None):
     db = g.pop('db', None)
